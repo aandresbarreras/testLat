a168e3cd5df5a24f9a799a25ed7257ebddbdc491
rolbee <rolbeeapp@gmail.com>
2024-10-11 00:52:32 -0500

    feature: adding secrests to python script

--- api/data/data.py
+++ api/data/data.py
@@ -1,16 +1,26 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+from pymongo.errors import ConnectionError
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://latmongo:mongolat@mongocluster.4akrr.mongodb.net/?retryWrites=true&w=majority&appName=mongoCluster")
-db = client['testLat']
-collection = db['dataMongo']
+mongo_uri = os.getenv('MONGODB_URI')
+
+try:
+    client = MongoClient(mongo_uri)
+    db = client['testLat']
+    collection = db['dataMongo']
+    print(f"Conectado a la base de datos.")
+except ConnectionError as e:
+    print("Error al conectar a la base de datos:", e)
 
 @app.route('/data', methods=['GET'])
-def get_data():  
-    data = list(collection.find({}, {'_id': 0}))
-    return jsonify(data)
+def get_data():
+    try:
+        data = list(collection.find({}, {'_id': 0}))
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)  
+    app.run(host='0.0.0.0', port=8081)
